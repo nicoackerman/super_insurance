4da2b832cdc8a9f379aed982fad62cc82236b2c5
nicoackerman <nicolascardenas0115@gmail.com>
2025-10-08 18:27:52 -0500

    added a home page for the entire app

--- super_insurance/urls.py
+++ super_insurance/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+
 from client import views
 from users import views
+from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('admin/', admin.site.urls),
     path('client/', include('client.urls')),
     path('users/', include('users.urls')),
--- users/views.py
+++ users/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 def login(request):
     return render(request, 'users/login.html')
 
 def signup(request):
-    return render(request, 'users/signup.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("client:home")
+    else:
+        form = UserCreationForm()
+    
+    return render(request, 'users/signup.html', {"form": form})
