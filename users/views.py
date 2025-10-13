1a7a2c6118206db56742042a098cce54dc2ce523
nicoackerman <nicolascardenas0115@gmail.com>
2025-10-13 12:59:11 -0500

    added full authentication

--- users/views.py
+++ users/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("home")
 
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect("client:home")
+            if 'next' in request.POST:
+                return redirect(request.POST.get('next'))
+            else:
+                return redirect("home")
         else:
             print(form.errors)
     else:
