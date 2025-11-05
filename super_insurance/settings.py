5e3ec2b071c5c7ac10d446e7fd04d96c07e79dc1
nicoackerman <nicolascardenas0115@gmail.com>
2025-11-05 14:02:50 -0500

    fix layout bugs

--- super_insurance/settings.py
+++ super_insurance/settings.py
@@ -131,6 +131,7 @@ LOCALE_PATHS = [
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
